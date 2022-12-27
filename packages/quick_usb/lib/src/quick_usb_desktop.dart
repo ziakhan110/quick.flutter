6413f443ef56a9c8a85e12cc3ffadc0544a7edfd
ziakhan110 <zia.khan110@gmail.com>
2022-12-27 10:22:29 +0500

    add m1 mac lib

--- packages/quick_usb/lib/src/quick_usb_desktop.dart
+++ packages/quick_usb/lib/src/quick_usb_desktop.dart
@@ -24,7 +24,11 @@ class QuickUsbMacos extends _QuickUsbDesktop {
   // For example/.dart_tool/flutter_build/generated_main.dart
   static registerWith() {
     QuickUsbPlatform.instance = QuickUsbMacos();
-    _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+    if (Platform.version.contains('arm64')) {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23_arm64.dylib'));
+    } else {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+    }
   }
 }
 
@@ -67,15 +71,13 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
     }
   }
 
-  Iterable<UsbDevice> _iterateDevice(
-      Pointer<Pointer<libusb_device>> deviceList) sync* {
+  Iterable<UsbDevice> _iterateDevice(Pointer<Pointer<libusb_device>> deviceList) sync* {
     var descPtr = ffi.calloc<libusb_device_descriptor>();
 
     for (var i = 0; deviceList[i] != nullptr; i++) {
       var dev = deviceList[i];
       var addr = _libusb.libusb_get_device_address(dev);
-      var getDesc = _libusb.libusb_get_device_descriptor(dev, descPtr) ==
-          libusb_error.LIBUSB_SUCCESS;
+      var getDesc = _libusb.libusb_get_device_descriptor(dev, descPtr) == libusb_error.LIBUSB_SUCCESS;
 
       yield UsbDevice(
         identifier: addr.toString(),
@@ -105,24 +107,20 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
     String? serialNumber;
     var descPtr = ffi.calloc<libusb_device_descriptor>();
     try {
-      var handle = _libusb.libusb_open_device_with_vid_pid(
-          nullptr, usbDevice.vendorId, usbDevice.productId);
+      var handle = _libusb.libusb_open_device_with_vid_pid(nullptr, usbDevice.vendorId, usbDevice.productId);
       if (handle != nullptr) {
         var device = _libusb.libusb_get_device(handle);
         if (device != nullptr) {
-          var getDesc = _libusb.libusb_get_device_descriptor(device, descPtr) ==
-              libusb_error.LIBUSB_SUCCESS;
+          var getDesc = _libusb.libusb_get_device_descriptor(device, descPtr) == libusb_error.LIBUSB_SUCCESS;
           if (getDesc) {
             if (descPtr.ref.iManufacturer > 0) {
-              manufacturer =
-                  _getStringDescriptorASCII(handle, descPtr.ref.iManufacturer);
+              manufacturer = _getStringDescriptorASCII(handle, descPtr.ref.iManufacturer);
             }
             if (descPtr.ref.iProduct > 0) {
               product = _getStringDescriptorASCII(handle, descPtr.ref.iProduct);
             }
             if (descPtr.ref.iSerialNumber > 0) {
-              serialNumber =
-                  _getStringDescriptorASCII(handle, descPtr.ref.iSerialNumber);
+              serialNumber = _getStringDescriptorASCII(handle, descPtr.ref.iSerialNumber);
             }
           }
         }
@@ -132,19 +130,14 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
       ffi.calloc.free(descPtr);
     }
     return UsbDeviceDescription(
-        device: usbDevice,
-        manufacturer: manufacturer,
-        product: product,
-        serialNumber: serialNumber);
+        device: usbDevice, manufacturer: manufacturer, product: product, serialNumber: serialNumber);
   }
 
-  String? _getStringDescriptorASCII(
-      Pointer<libusb_device_handle> handle, int descIndex) {
+  String? _getStringDescriptorASCII(Pointer<libusb_device_handle> handle, int descIndex) {
     String? result;
     Pointer<ffi.Utf8> string = ffi.calloc<Uint8>(256).cast();
     try {
-      var ret = _libusb.libusb_get_string_descriptor_ascii(
-          handle, descIndex, string.cast(), 256);
+      var ret = _libusb.libusb_get_string_descriptor_ascii(handle, descIndex, string.cast(), 256);
       if (ret > 0) {
         result = string.toDartString();
       }
@@ -168,8 +161,7 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
   Future<bool> openDevice(UsbDevice usbDevice) async {
     assert(_devHandle == null, 'Last device not closed');
 
-    var handle = _libusb.libusb_open_device_with_vid_pid(
-        nullptr, usbDevice.vendorId, usbDevice.productId);
+    var handle = _libusb.libusb_open_device_with_vid_pid(nullptr, usbDevice.vendorId, usbDevice.productId);
     if (handle == nullptr) {
       return false;
     }
@@ -192,8 +184,7 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
     var configDescPtrPtr = ffi.calloc<Pointer<libusb_config_descriptor>>();
     try {
       var device = _libusb.libusb_get_device(_devHandle!);
-      var getConfigDesc =
-          _libusb.libusb_get_config_descriptor(device, index, configDescPtrPtr);
+      var getConfigDesc = _libusb.libusb_get_config_descriptor(device, index, configDescPtrPtr);
       if (getConfigDesc != libusb_error.LIBUSB_SUCCESS) {
         throw 'getConfigDesc error: ${_libusb.describeError(getConfigDesc)}';
       }
@@ -202,9 +193,7 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
       var usbConfiguration = UsbConfiguration(
         id: configDescPtr.ref.bConfigurationValue,
         index: configDescPtr.ref.iConfiguration,
-        interfaces: _iterateInterface(
-                configDescPtr.ref.interface_1, configDescPtr.ref.bNumInterfaces)
-            .toList(),
+        interfaces: _iterateInterface(configDescPtr.ref.interface_1, configDescPtr.ref.bNumInterfaces).toList(),
       );
       _libusb.libusb_free_config_descriptor(configDescPtr);
 
@@ -214,8 +203,7 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
     }
   }
 
-  Iterable<UsbInterface> _iterateInterface(
-      Pointer<libusb_interface> interfacePtr, int interfaceCount) sync* {
+  Iterable<UsbInterface> _iterateInterface(Pointer<libusb_interface> interfacePtr, int interfaceCount) sync* {
     for (var i = 0; i < interfaceCount; i++) {
       var interface = interfacePtr[i];
       for (var j = 0; j < interface.num_altsetting; j++) {
@@ -223,16 +211,13 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
         yield UsbInterface(
           id: intfDesc.bInterfaceNumber,
           alternateSetting: intfDesc.bAlternateSetting,
-          endpoints: _iterateEndpoint(intfDesc.endpoint, intfDesc.bNumEndpoints)
-              .toList(),
+          endpoints: _iterateEndpoint(intfDesc.endpoint, intfDesc.bNumEndpoints).toList(),
         );
       }
     }
   }
 
-  Iterable<UsbEndpoint> _iterateEndpoint(
-      Pointer<libusb_endpoint_descriptor> endpointDescPtr,
-      int endpointCount) sync* {
+  Iterable<UsbEndpoint> _iterateEndpoint(Pointer<libusb_endpoint_descriptor> endpointDescPtr, int endpointCount) sync* {
     for (var i = 0; i < endpointCount; i++) {
       var endpointDesc = endpointDescPtr[i];
       yield UsbEndpoint(
@@ -279,11 +264,9 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
   }
 
   @override
-  Future<Uint8List> bulkTransferIn(
-      UsbEndpoint endpoint, int maxLength, int timeout) async {
+  Future<Uint8List> bulkTransferIn(UsbEndpoint endpoint, int maxLength, int timeout) async {
     assert(_devHandle != null, 'Device not open');
-    assert(endpoint.direction == UsbEndpoint.DIRECTION_IN,
-        'Endpoint\'s direction should be in');
+    assert(endpoint.direction == UsbEndpoint.DIRECTION_IN, 'Endpoint\'s direction should be in');
 
     var actualLengthPtr = ffi.calloc<Int32>();
     var dataPtr = ffi.calloc<Uint8>(maxLength);
@@ -308,11 +291,9 @@ class _QuickUsbDesktop extends QuickUsbPlatform {
   }
 
   @override
-  Future<int> bulkTransferOut(
-      UsbEndpoint endpoint, Uint8List data, int timemout) async {
+  Future<int> bulkTransferOut(UsbEndpoint endpoint, Uint8List data, int timemout) async {
     assert(_devHandle != null, 'Device not open');
-    assert(endpoint.direction == UsbEndpoint.DIRECTION_OUT,
-        'Endpoint\'s direction should be out');
+    assert(endpoint.direction == UsbEndpoint.DIRECTION_OUT, 'Endpoint\'s direction should be out');
 
     var actualLengthPtr = ffi.calloc<Int32>();
     var dataPtr = ffi.calloc<Uint8>(data.length);
--- packages/quick_usb/macos/quick_usb.podspec
+++ packages/quick_usb/macos/quick_usb.podspec
@@ -16,7 +16,7 @@ A new flutter plugin project.
   s.source_files     = 'Classes/**/*'
   s.dependency 'FlutterMacOS'
 
-  s.vendored_libraries = 'libusb-1.0.23.dylib'
+  s.vendored_libraries = 'libusb-1.0.23.dylib', 'libusb-1.0.23_arm64.dylib' 
 
   s.platform = :osx, '10.11'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
